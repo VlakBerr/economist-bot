f7346e664641e6b56f6d0d23a1d3fc18a1664dfd
VlakBerr <boro46141@gmail.com>
2025-06-12 13:37:22 +0300

    исправил грамматичсекие ошибки в коде

--- schema.sql
+++ schema.sql
@@ -2,27 +2,26 @@ CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
-)
+);
 
-CREATE TABLE IF NOT EXISTS incomes (
-   user_id INTEGER NOT NULL,       
-   income TEXT NOT NULL,
-   title TEXT NOT NULL,
-   created_at TEXT NOT NULL DEFAULT date('now'),
-   FOREIGN KEY (user_id) REFERENCES users (id)
-)
-
-CREATE TABLE IF NOT EXISTS expenses (
-   user_id INTEGER NOT NULL,       
-   expense TEXT NOT NULL,
-   title TEXT NOT NULL,
-   created_at TEXT NOT NULL DEFAULT date('now'),
-   FOREIGN KEY (user_id) REFERENCES users (id)
-)
-
-CREATE TABLE IF NOT EXISTS goals (
+CREATE TABLE IF NOT EXISTS incomes(
    user_id INTEGER NOT NULL,
-   title TEXT NOT NULL UNIQUE,
+   title TEXT NOT NULL,       
+   income TEXT NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS expenses(
+   user_id INTEGER NOT NULL,
+   title TEXT NOT NULL,       
+   expense TEXT NOT NULL,
+   created_at DATETIME NOT NULL DEFAULT current_timestamp,
+   FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS goals(
+   user_id INTEGER NOT NULL,
+   title TEXT NOT NULL,
    money_count TEXT NOT NULL,
    savings TEXT NOT NULL DEFAULT '0',
    FOREIGN KEY (user_id) REFERENCES users (id)
